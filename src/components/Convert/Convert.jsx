773fb1d7c0334a46fb2844f235f0a98df28e1d38
Guido Ortiz <guidoortiz93@hotmail.com>
2022-11-26 13:24:38 +0100

    styles convert comp

--- src/components/Convert/Convert.jsx
+++ src/components/Convert/Convert.jsx
@@ -1,12 +1,36 @@
+import { SyncAlt, FavoriteBorder } from '@mui/icons-material';
 import React from 'react';
 import s from './Convert.module.css';
 
 const Convert = () => {
   return (
     <div className={s.convert__container}>
+
         <div className={s.convert__title}>convert</div>
-        <input />
-        <input />
+
+        <div className={s.convert__select__container}>
+          <select className={s.convert__select}>
+            <option className={s.convert__select__option}>km to miles</option>
+          </select>
+          <div className={s.convert__select__icon}>
+            <SyncAlt />
+          </div>
+        </div>
+
+        <div className={s.convert__input__container}>
+          <input className={s.convert__input} value={32} />
+          <div className={s.convert__input__unit}>km</div>
+        </div>
+
+        <div className={s.convert__result__container}>
+          <div className={s.convert__result__icon}>
+            <FavoriteBorder />
+          </div>
+          <div className={s.convert__results}>
+            <div className={s.convert__result}>62.12</div>
+            <div className={s.convert__result__unit}>miles</div>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import s from './Topbar.module.css';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
 
 const Topbar = () => {
   return (
     <div className={s.topbar}>
-        {/* <span className={s.topbar__logo}>ho</span> */}
+        <span className={s.topbar__logo}>
+          <SyncAltIcon />
+        </span>
         <div className={s.topbar__title}>unit converter</div>
     </div>
   )
